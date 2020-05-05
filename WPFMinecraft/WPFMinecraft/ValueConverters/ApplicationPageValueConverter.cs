5b5bf2df2cecf8421baaf57c5e40ae8d0eb98ea4
godofsound <robbe.baeyens@outlook.com>
2020-05-05 16:05:49 +0200

    window style & hoofdpagina

---
+++ WPFMinecraft/WPFMinecraft/ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using WPFMinecraft.Pages;
+
+namespace WPFMinecraft.ValueConverters
+{
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Home:
+                    return new HomescreenPage();
+
+
+                case ApplicationPage.Player:
+                    return new PlayerPage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
