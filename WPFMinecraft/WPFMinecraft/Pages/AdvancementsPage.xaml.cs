a900edebb711e1745713d360898e8e8723f293cd
Bryan Antonis <bryan-antonis@hotmail.com>
2020-05-25 20:02:30 +0200

    Advancement-scherm interactief gemaakt

--- WPFMinecraft/WPFMinecraft/Pages/AdvancementsPage.xaml.cs
+++ WPFMinecraft/WPFMinecraft/Pages/AdvancementsPage.xaml.cs
@@ -25,9 +25,210 @@ namespace WPFMinecraft.Pages
             InitializeComponent();
         }
 
-        private void Canvas_Loaded(object sender, RoutedEventArgs e)
+        private void btnGrantAdvancement_Click(object sender, RoutedEventArgs e)
         {
+            Style grey = (Style)FindResource("normalAdvancementGrey");
+            Style specialgrey = (Style)FindResource("specialAdvancementGrey");
 
+
+            if (btnAdvancementBetoverdBoek.IsChecked == true)
+            {
+                btnAdvancementBetoverdBoek.Style = grey;
+                btnAdvancementBetoverdBoek.IsChecked = false;
+            }
+
+            if (btnAdvancementDiamant.IsChecked == true)
+            {
+                btnAdvancementDiamant.Style = grey;
+                btnAdvancementDiamant.IsChecked = false;
+            }
+
+            if (btnAdvancementDiamantenBorst.IsChecked == true)
+            {
+                btnAdvancementDiamantenBorst.Style = grey;
+                btnAdvancementDiamantenBorst.IsChecked = false;
+            }
+
+            if (btnAdvancementEindeSteen.IsChecked == true)
+            {
+                btnAdvancementEindeSteen.Style = grey;
+                btnAdvancementEindeSteen.IsChecked = false;
+            }
+
+            if (btnAdvancementEmmerLava.IsChecked == true)
+            {
+                btnAdvancementEmmerLava.Style = grey;
+                btnAdvancementEmmerLava.IsChecked = false;
+            }
+
+            if (btnAdvancementEnderOog.IsChecked == true)
+            {
+                btnAdvancementEnderOog.Style = grey;
+                btnAdvancementEnderOog.IsChecked = false;
+            }
+
+            if (btnAdvancementGoudenAppel.IsChecked == true)
+            {
+                btnAdvancementGoudenAppel.Style = specialgrey;
+                btnAdvancementGoudenAppel.IsChecked = false;
+            }
+
+            if (btnAdvancementGrasBlok.IsChecked == true)
+            {
+                btnAdvancementGrasBlok.Style = grey;
+                btnAdvancementGrasBlok.IsChecked = false;
+            }
+
+            if (btnAdvancementHoutenPikhouweel.IsChecked == true)
+            {
+                btnAdvancementHoutenPikhouweel.Style = grey;
+                btnAdvancementHoutenPikhouweel.IsChecked = false;
+            }
+
+            if (btnAdvancementHoutenSchild.IsChecked == true)
+            {
+                btnAdvancementHoutenSchild.Style = grey;
+                btnAdvancementHoutenSchild.IsChecked = false;
+            }
+
+            if (btnAdvancementIjzerBorst.IsChecked == true)
+            {
+                btnAdvancementIjzerBorst.Style = grey;
+                btnAdvancementIjzerBorst.IsChecked = false;
+            }
+
+            if (btnAdvancementIjzerenPikhouweel.IsChecked == true)
+            {
+                btnAdvancementIjzerenPikhouweel.Style = grey;
+                btnAdvancementIjzerenPikhouweel.IsChecked = false;
+            }
+
+            if (btnAdvancementIjzerKlomp.IsChecked == true)
+            {
+                btnAdvancementIjzerKlomp.Style = grey;
+                btnAdvancementIjzerKlomp.IsChecked = false;
+            }
+
+            if (btnAdvancementObsidiaan.IsChecked == true)
+            {
+                btnAdvancementObsidiaan.Style = grey;
+                btnAdvancementObsidiaan.IsChecked = false;
+            }
+
+            if (btnAdvancementStenenPikhouweel.IsChecked == true)
+            {
+                btnAdvancementStenenPikhouweel.Style = grey;
+                btnAdvancementStenenPikhouweel.IsChecked = false;
+            }
+
+            if (btnAdvancementVuursteen.IsChecked == true)
+            {
+                btnAdvancementVuursteen.Style = grey;
+                btnAdvancementVuursteen.IsChecked = false;
+            }
+        }
+
+
+        private void btnRevokeAdvancement_Click(object sender, RoutedEventArgs e)
+        {
+            Style brown = (Style)FindResource("normalAdvancementBrown");
+            Style specialbrown = (Style)FindResource("specialAdvancementBrown");
+
+            if (btnAdvancementBetoverdBoek.IsChecked == true)
+            {
+                btnAdvancementBetoverdBoek.Style = brown;
+                btnAdvancementBetoverdBoek.IsChecked = false;
+            }
+
+            if (btnAdvancementDiamant.IsChecked == true)
+            {
+                btnAdvancementDiamant.Style = brown;
+                btnAdvancementDiamant.IsChecked = false;
+            }
+
+            if (btnAdvancementDiamantenBorst.IsChecked == true)
+            {
+                btnAdvancementDiamantenBorst.Style = brown;
+                btnAdvancementDiamantenBorst.IsChecked = false;
+            }
+
+            if (btnAdvancementEindeSteen.IsChecked == true)
+            {
+                btnAdvancementEindeSteen.Style = brown;
+                btnAdvancementEindeSteen.IsChecked = false;
+            }
+
+            if (btnAdvancementEmmerLava.IsChecked == true)
+            {
+                btnAdvancementEmmerLava.Style = brown;
+                btnAdvancementEmmerLava.IsChecked = false;
+            }
+
+            if (btnAdvancementEnderOog.IsChecked == true)
+            {
+                btnAdvancementEnderOog.Style = brown;
+                btnAdvancementEnderOog.IsChecked = false;
+            }
+
+            if (btnAdvancementGoudenAppel.IsChecked == true)
+            {
+                btnAdvancementGoudenAppel.Style = specialbrown;
+                btnAdvancementGoudenAppel.IsChecked = false;
+            }
+
+            if (btnAdvancementGrasBlok.IsChecked == true)
+            {
+                btnAdvancementGrasBlok.Style = brown;
+                btnAdvancementGrasBlok.IsChecked = false;
+            }
+
+            if (btnAdvancementHoutenPikhouweel.IsChecked == true)
+            {
+                btnAdvancementHoutenPikhouweel.Style = brown;
+                btnAdvancementHoutenPikhouweel.IsChecked = false;
+            }
+
+            if (btnAdvancementHoutenSchild.IsChecked == true)
+            {
+                btnAdvancementHoutenSchild.Style = brown;
+                btnAdvancementHoutenSchild.IsChecked = false;
+            }
+
+            if (btnAdvancementIjzerBorst.IsChecked == true)
+            {
+                btnAdvancementIjzerBorst.Style = brown;
+                btnAdvancementIjzerBorst.IsChecked = false;
+            }
+
+            if (btnAdvancementIjzerenPikhouweel.IsChecked == true)
+            {
+                btnAdvancementIjzerenPikhouweel.Style = brown;
+                btnAdvancementIjzerenPikhouweel.IsChecked = false;
+            }
+
+            if (btnAdvancementIjzerKlomp.IsChecked == true)
+            {
+                btnAdvancementIjzerKlomp.Style = brown;
+                btnAdvancementIjzerKlomp.IsChecked = false;
+            }
+
+            if (btnAdvancementObsidiaan.IsChecked == true)
+            {
+                btnAdvancementObsidiaan.Style = brown;
+                btnAdvancementObsidiaan.IsChecked = false;
+            }
+
+            if (btnAdvancementStenenPikhouweel.IsChecked == true)
+            {
+                btnAdvancementStenenPikhouweel.Style = brown;
+                btnAdvancementStenenPikhouweel.IsChecked = false;
+            }
+
+            if (btnAdvancementVuursteen.IsChecked == true)
+            {
+                btnAdvancementVuursteen.Style = brown;
+                btnAdvancementVuursteen.IsChecked = false;
+            }
         }
     }
 }
