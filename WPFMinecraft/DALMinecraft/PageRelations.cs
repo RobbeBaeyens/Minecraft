857951589e4c5f670b43c20c77b2390f52e86524
godofsound <robbe.baeyens@outlook.com>
2020-05-27 23:43:07 +0200

    page relations

---
+++ WPFMinecraft/DALMinecraft/PageRelations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALMinecraft
+{
+    public class PageRelations
+    {
+        public int serverId { get; set; }
+        public int worldId { get; set; }
+        public int playerId { get; set; }
+        public int inventoryId { get; set; }
+    }
+}
