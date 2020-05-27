f9226a5649d4f7dfd1efdf567c69b9b61abb90f4
godofsound <robbe.baeyens@outlook.com>
2020-05-28 01:27:24 +0200

    removed ModelsMinecraft and added a way to move variables trough pages

---
+++ WPFMinecraft/ModelsMinecraft/PageRelationsServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALMinecraft;
+
+namespace ModelsMinecraft
+{
+    public class PageRelationsServer
+    {
+        public int ServerId { get; set; }
+
+        public PageRelationsServer(int serverId)
+        {
+            this.ServerId = serverId;
+        }
+    }
+}
