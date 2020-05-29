3c24177f8c3115ecf171656a713df13eaaafcb6b
godofsound <robbe.baeyens@outlook.com>
2020-05-29 02:52:10 +0200

    player_page

--- WPFMinecraft/WPFMinecraft/Pages/PlayerManagementPage.xaml.cs
+++ WPFMinecraft/WPFMinecraft/Pages/PlayerManagementPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using DALMinecraft;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
--- WPFMinecraft/WPFMinecraft/Pages/PlayerPage.xaml.cs
+++ WPFMinecraft/WPFMinecraft/Pages/PlayerPage.xaml.cs
@@ -26,6 +26,8 @@ namespace WPFMinecraft.Pages
         public int worldId;
         public int playerId;
         public Player player;
+        public World world;
+        public Server server;
 
         public PlayerPage()
         {
@@ -55,62 +57,43 @@ namespace WPFMinecraft.Pages
             }
 
             player = DatabaseOperations.OphalenSpeler(playerId);
+            world = DatabaseOperations.OphalenWorldViaSpeler(playerId);
+            server = DatabaseOperations.OphalenServer(worldId);
             lblPlayerNameTag.Content = player.name;
             lblPlayerName.Content = player.name;
             lblPlayerUUID.Content = player.uuid;
-            lblPlayerCoords.Content = "x:" + player.posX + " y:" +player.posY + " z:" + player.posZ;
+            lblPlayerCoords.Content = "x:" + Math.Round(player.posX,0) + " y:" + Math.Round(player.posY, 0) + " z:" + Math.Round(player.posZ, 0);
             lblPlayerNameTag.Content = player.name;
+            lblWorldSeed.Content = world.seed;
+            lblWorldName.Content = world.name;
+            lblServerNameAndIP.Content = server.name + "   -   " + server.ipadress;
+
+            drawStats(0);
+            drawStats(1);
+            drawStats(2);
         }
 
         private void btnEffects_Click(object sender, RoutedEventArgs e)
         {
-            // Find the frame.
-            Frame pageFrame = null;
-            DependencyObject currParent = VisualTreeHelper.GetParent(this);
-
-            while (currParent != null && pageFrame == null)
-            {
-                pageFrame = currParent as Frame;
-                currParent = VisualTreeHelper.GetParent(currParent);
-            }
-
-            //Change the page of the frame.
-            if (pageFrame.DataContext != null)
-            {
-                WindowViewModel windowViewModel = pageFrame.DataContext as WindowViewModel;
-                windowViewModel.CurrentPage = ApplicationPage.Effects;
-                windowViewModel.WorldId = worldId;
-                windowViewModel.PlayerId = playerId;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Server ID: " + serverId + "\nWorld ID: " + worldId + "\nPlayer ID: " + serverId + "\n");
-            }
+            goToNewPage(ApplicationPage.Effects);
         }
 
         private void btnAdvancements_Click(object sender, RoutedEventArgs e)
         {
-            // Find the frame.
-            Frame pageFrame = null;
-            DependencyObject currParent = VisualTreeHelper.GetParent(this);
-
-            while (currParent != null && pageFrame == null)
-            {
-                pageFrame = currParent as Frame;
-                currParent = VisualTreeHelper.GetParent(currParent);
-            }
-
-            //Change the page of the frame.
-            if (pageFrame.DataContext != null)
-            {
-                WindowViewModel windowViewModel = pageFrame.DataContext as WindowViewModel;
-                windowViewModel.CurrentPage = ApplicationPage.Advancements;
-                windowViewModel.WorldId = worldId;
-                windowViewModel.PlayerId = playerId;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Server ID: " + serverId + "\nWorld ID: " + worldId + "\nPlayer ID: " + serverId + "\n");
-            }
+            goToNewPage(ApplicationPage.Advancements);
         }
 
         private void btnRecipes_Click(object sender, RoutedEventArgs e)
+        {
+            goToNewPage(ApplicationPage.Recipes);
+        }
+
+        private void btnInventory_Click(object sender, RoutedEventArgs e)
+        {
+            goToNewPage(ApplicationPage.Inventory);
+        }
+
+        public void goToNewPage(ApplicationPage applicationPage)
         {
             // Find the frame.
             Frame pageFrame = null;
@@ -126,30 +109,7 @@ namespace WPFMinecraft.Pages
             if (pageFrame.DataContext != null)
             {
                 WindowViewModel windowViewModel = pageFrame.DataContext as WindowViewModel;
-                windowViewModel.CurrentPage = ApplicationPage.Recipes;
-                windowViewModel.WorldId = worldId;
-                windowViewModel.PlayerId = playerId;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Server ID: " + serverId + "\nWorld ID: " + worldId + "\nPlayer ID: " + serverId + "\n");
-            }
-        }
-
-        private void btnInventory_Click(object sender, RoutedEventArgs e)
-        {// Find the frame.
-            Frame pageFrame = null;
-            DependencyObject currParent = VisualTreeHelper.GetParent(this);
-
-            while (currParent != null && pageFrame == null)
-            {
-                pageFrame = currParent as Frame;
-                currParent = VisualTreeHelper.GetParent(currParent);
-            }
-
-            //Change the page of the frame.
-            if (pageFrame.DataContext != null)
-            {
-                WindowViewModel windowViewModel = pageFrame.DataContext as WindowViewModel;
-                windowViewModel.CurrentPage = ApplicationPage.Inventory;
+                windowViewModel.CurrentPage = applicationPage;
                 windowViewModel.WorldId = worldId;
                 windowViewModel.PlayerId = playerId;
                 Console.ForegroundColor = ConsoleColor.Yellow;
@@ -160,6 +120,101 @@ namespace WPFMinecraft.Pages
         private void btnFlip_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void btnHealthMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.health > 0)
+            {
+                player.health--;
+                drawStats(0);
+            }
+        }
+
+        private void btnHealthPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.health < 20)
+            { 
+                player.health++;
+                drawStats(0);
+            }
+        }
+
+        private void btnFoodMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.food > 0)
+            {
+                player.food--;
+                drawStats(1);
+            }
+        }
+
+        private void btnFoodPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.food < 20)
+            {
+                player.food++;
+                drawStats(1);
+            }
+        }
+
+        private void btnArmorMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.armor > 0)
+            {
+                player.armor--;
+                drawStats(2);
+            }
+        }
+
+        private void btnArmorPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.armor < 20)
+            {
+                player.armor++;
+                drawStats(2);
+            }
+        }
+
+        public void drawStats(int type)
+        {
+            DatabaseOperations.UpdateSpeler(player);
+            List<StackPanel> statHolders    = new List<StackPanel>(){ stckpnlPlayerHealth,  stckpnlPlayerFood,  stckpnlPlayerArmor  };
+            List<string> ImagePartialPaths  = new List<string>()    { "Health/health",      "Food/food",        "Armor/armor"       };
+            double health  = (double)player.health;
+            double food    = (double)player.food;
+            double armor   = (double)player.armor;
+            string startImagePath   = "pack://application:,,,/Images/Player/" + ImagePartialPaths[type];
+            var ImagePath_Empty     = new Uri(startImagePath + "_empty.png");
+            var ImagePath_Half      = new Uri(startImagePath + "_half.png");
+            var ImagePath_Full      = new Uri(startImagePath + "_full.png");
+
+            List<int> Stat_Halfs = new List<int>() { (int)health % 2, (int)food % 2, (int)armor % 2 };
+            List<int> Stat_Fulls = new List<int>() { (int)Math.Floor(health / 2), (int)Math.Floor(food / 2), (int)Math.Floor(armor / 2) };
+            List<int> Stat_Empties = new List<int>() { 10 - Stat_Fulls[0] - Stat_Halfs[0], 10 - Stat_Fulls[1] - Stat_Halfs[1], 10 - Stat_Fulls[2] - Stat_Halfs[2] };
+
+            statHolders[type].Children.Clear();
+            for (int i = 0; i < Stat_Fulls[type]; i++)
+            {
+                Image image = new Image();
+                image.Source = new BitmapImage(ImagePath_Full);
+                image.Width = 15;
+                statHolders[type].Children.Add(image);
+            }
+            for (int i = 0; i < Stat_Halfs[type]; i++)
+            {
+                Image image = new Image();
+                image.Source = new BitmapImage(ImagePath_Half);
+                image.Width = 15;
+                statHolders[type].Children.Add(image);
+            }
+            for (int i = 0; i < Stat_Empties[type]; i++)
+            {
+                Image image = new Image();
+                image.Source = new BitmapImage(ImagePath_Empty);
+                image.Width = 15;
+                statHolders[type].Children.Add(image);
+            }
         }
     }
 }
