1f778faab2bee1ec64a8e97d870fcc6e0a883aab
Bryan Antonis <bryan-antonis@hotmail.com>
2020-05-29 14:10:33 +0200

    aanpassingen

--- WPFMinecraft/WPFMinecraft/Pages/InventoryManagerPage.xaml.cs
+++ WPFMinecraft/WPFMinecraft/Pages/InventoryManagerPage.xaml.cs
@@ -75,7 +75,22 @@ namespace WPFMinecraft.Pages
 
             cmb1.ItemsSource = invItems;
 
-            
+            List<string> slots = new List<string>()
+            {
+                "armor.helmet" ,
+                "armor.chestplate",
+                "armor.leggings",
+                "armor.boots",
+                "inv.offhand",
+
+                "hotbar.0", "hotbar.1", "hotbar.2", "hotbar.3", "hotbar.4",  "hotbar.5",  "hotbar.6", "hotbar.7",  "hotbar.8",
+
+                "inv.0", "inv.1", "inv.2", "inv.3", "inv.4",  "inv.5",  "inv.6", "inv.7",  "inv.8",
+                "inv.9", "inv.10", "inv.11", "inv.12", "inv.13",  "inv.14",  "inv.15", "inv.16",  "inv.17",
+                "inv.18", "inv.19", "inv.20", "inv.21", "inv.22",  "inv.23",  "inv.24", "inv.25",  "inv.26",
+            };
+
+            cmb2.ItemsSource = slots;
             
 
         }
