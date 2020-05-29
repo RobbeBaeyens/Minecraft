673b4eeca3dd41394c11795c7d51d65d0735fda7
Andreas <r0771721@student.thomasmore.be>
2020-05-29 11:10:54 +0200

    Game Rule pagina afgewerkt

--- WPFMinecraft/WPFMinecraft/Pages/GameRulePage.xaml.cs
+++ WPFMinecraft/WPFMinecraft/Pages/GameRulePage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DALMinecraft;
+using WPFMinecraft.Properties;
 using WPFMinecraft.ViewModel;
 
 namespace WPFMinecraft.Pages
@@ -26,12 +27,16 @@ namespace WPFMinecraft.Pages
         {
             InitializeComponent();
         }
+
         public int serverId;
         public int worldId;
-       
 
-        public List<Button> settingButton;
-        World world = new World();
+        
+        public List<Button> settingButtons;
+        public List<string> buttonNames;
+        public World world;
+        
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             // Find the frame.
@@ -49,8 +54,9 @@ namespace WPFMinecraft.Pages
             {
                 WindowViewModel windowViewModel = pageFrame.DataContext as WindowViewModel;
                 serverId = windowViewModel.ServerId;
+                worldId = windowViewModel.WorldId;
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Server ID: " + serverId + "\n");
+                Console.WriteLine("Server ID: " + serverId + "\nWorld ID:" + worldId );
             }
 
 
@@ -58,165 +64,56 @@ namespace WPFMinecraft.Pages
 
 
 
-            settingButton = new List<Button>()
+            settingButtons = new List<Button>()
             {
                 btnEltyraMovement, btnRespawnImmediatly ,btnRecipeCrafting, btnDrowningDmg, btnFallDmg, btnFireDmg, btnKeepInv, btnHealthRegen,
                 btnGenerateTerrain, btnDisableRaids, btnAllowDestructiveMob, btnSpawnPhantoms, btnSpawnMobs, btnPillagerPatrol, btnWanderingTraders,
                 btnEntetyEquipment, btnMobLoot, btnDropBlocks, btnInGameTime, btnUpdateFire, btnUpdateWeather, btnAnnounceAdvancements, btnBlockOutput,
                 btnAdminCommands, btnFeedback, btnDeathMessages, btnDebugInfo
             };
-            LoadGameRules();
+
+            buttonNames = new List<string>()
+            {
+                "Disable elytra movement check: ", "Respawn immediatly: ", "Require recipe for crafting: ", "Deal drowning damage: ", "Deal fall damage: ","Deal fire damage: ",
+                "Keep inventory after death: ", "Regenerate health: ", "Allow spectators to generate terrain: ", "Disable raids: ", "Allow destructive mob actions: ",
+                "Spawn phantoms: ", "Spawn mobs: ", "Spawn pillager patrols: ", "Spawn wandering traders: ", "Drop entity equipment: ", "Drop mob loot: ", "Drop blocks: ",
+                "Advance in-game time: ", "Update fire: ", "Update weather: ", "Announce advancements: ", "Broadcast command block output: ", "Broadcast admin commands: ",
+                "Send command feedback: ", "Show death messages: ", "Reduce debug info: "
+            };
+
+            LoadGameRules(null);
             
         }
 
-        public void LoadGameRules()
+        public void LoadGameRules(Button sender)
         {
+            world = DatabaseOperations.OphalenSettings(worldId);
+           
+            
             var count = 0;
-
-            if (settingButton[count].IsEnabled == true )
+            foreach (World_Setting worldsetting in world.World_Setting)
             {
+                if (sender == settingButtons[count] && worldsetting.value)
+                {
+                    worldsetting.value = false;
+                    DatabaseOperations.UpdateWorldSetting(worldsetting);
 
+                }
+                else if (sender == settingButtons[count] && !worldsetting.value)
+                {
+                    worldsetting.value = true;
+                    DatabaseOperations.UpdateWorldSetting(worldsetting);
+                }
+                settingButtons[count].Content = buttonNames[count] + (worldsetting.value ? "ON" : "OFF");
+                count++;
             }
 
-            count++;
-        }
-        private void btnElytraMovement_Click(object sender, RoutedEventArgs e)
-        {
-
-            
-
-        }
-
-        private void btnRespawnImmediatly_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void btnRecipeCrafting_Click(object sender, RoutedEventArgs e)
-        {
             
         }
 
-        private void btnDrowningDmg_Click(object sender, RoutedEventArgs e)
+        private void btnGameRuleToggle_Click(object sender, RoutedEventArgs e)
         {
-            
+            LoadGameRules(sender as Button);
         }
-
-        private void btnFallDmg_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void btnFireDmg_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnKeepInv_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnHealthRegen_Click(object sender, RoutedEventArgs e)
-        {
-          
-        }
-
-        private void btnGenerateTerrain_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnDisableRaids_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnAllowDestructiveMob_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnSpawnPhantoms_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnSpawnMobs_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void btnPillagerPatrol_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnWanderingTraders_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnEntetyEquipment_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnMobLoot_Click(object sender, RoutedEventArgs e)
-        {
-          
-        }
-
-        private void btnDropBlocks_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnInGameTime_Click(object sender, RoutedEventArgs e)
-        {
-          
-        }
-
-        private void btnUpdateFire_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void btnUpdateWeather_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnAnnounceAdvancements_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnBlockOutput_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnAdminCommands_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void btnFeedback_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void btnDeathMessages_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void btnDebugInfo_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        
     }
 }
